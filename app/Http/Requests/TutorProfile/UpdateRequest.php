98f1c684413fba94fd38ea025840c3515ec7a0f3
md christien <mdchristien12@gmail.com>
2025-01-22 09:29:36 +0100

    refactor tutor profile update request

--- app/Http/Requests/TutorProfile/UpdateRequest.php
+++ app/Http/Requests/TutorProfile/UpdateRequest.php
@@ -11,7 +11,7 @@ class UpdateRequest extends FormRequest
      */
      public function authorize(): bool
     {
-        return $this->user()->isTutor();
+        return $this->user() && $this->user()->isTutor();
     }
 
     protected function failedAuthorization()
